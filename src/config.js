72b7a92e35cbd89d28a4d9fd63f4cad4db4f656c
kavilla <kavilla@wisc.edu>
2017-06-20 20:22:51 -0500

    login successful

--- src/config.js
+++ src/config.js
@@ -23,7 +23,7 @@ const config = {
   all: {
     env: process.env.NODE_ENV || 'development',
     root: path.join(__dirname, '..'),
-    port: process.env.PORT || 9000,
+    port: process.env.PORT || 3306,
     ip: process.env.IP || '0.0.0.0',
     masterKey: requireProcessEnv('MASTER_KEY'),
     jwtSecret: requireProcessEnv('JWT_SECRET'),
